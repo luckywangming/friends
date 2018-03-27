5db33993ed0dfc60d73a281d47a53505edcb87c6
wang <wangminglucky@sina.com>
2018-03-27 18:56:55 +0800

    swiper

--- fr/src/common/swiper/swiper.vue
+++ fr/src/common/swiper/swiper.vue
@@ -1,45 +1,49 @@
 <template>
-  <swiper :options='swiperOption'>
-    <swiper-slide v-for="item in data" v-bind:key="item" >
-      <img :src="item" alt="">
-    </swiper-slide>
-    <div v-if="data.length > 1" class="swiper-pagination"  slot="pagination"></div>
-  </swiper>
+  <div>
+    <mt-swipe :speed="0">
+      <mt-swipe-item v-for="item in data" :key="item+Math.random()">
+        <img :src="item" alt="">
+      </mt-swipe-item>
+    </mt-swipe>
+  </div>
 </template>
 <script>
-import { swiper, swiperSlide } from "vue-awesome-swiper";
+import { Swipe, SwipeItem } from "mint-ui";
 export default {
   props: {
     data: {
       type: Array,
-      default:function(){
-        return ["static/image/index-banner.jpg"]
+      default: function() {
+        return ["static/image/index-banner.jpg","static/image/index-banner.jpg"];
       }
     }
   },
   components: {
-    swiper,
-    swiperSlide
+    Swipe,
+    SwipeItem
   },
   data() {
     return {
       swiperOption: {
         autoplay: 3000,
-        direction : 'horizontal',
-        grabCursor : true,
-        setWrapperSize :true,
+        direction: "horizontal",
+        grabCursor: true,
+        setWrapperSize: true,
         autoHeight: true,
-        pagination : '.swiper-pagination',
-        paginationClickable :true,
-        mousewheelControl : true,
-        observeParents:true
+        pagination: ".swiper-pagination",
+        paginationClickable: true,
+        mousewheelControl: true,
+        observeParents: true
       }
     };
   }
 };
 </script>
 <style lang="stylus" scoped>
-.swiper-slide {
+.mint-swipe{
+  height:300px;
+}
+.mint-swipe-item {
   text-align: center;
   height: 300px;
   line-height: 300px;
